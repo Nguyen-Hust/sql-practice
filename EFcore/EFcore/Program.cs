2d1926d00e686dc58f7dd4bed54e38115af4bb1d
ThanhDat <thanhdattpa5@gmail.com>
2021-03-31 18:24:29 +0700

    Upload T-SQL and EFcore folder

---
+++ EFcore/EFcore/Program.cs
@@ -0,0 +1,18 @@
+﻿using EFcore.BLL;
+using System;
+using System.Linq;
+
+namespace EFcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BikeStoreDbContext())
+            {
+                Lession1 lession = new Lession1(context);
+                lession.ShowProductOfStore();
+            }
+        }
+    }
+}
